2dd1d552f7c46873b8a847f85ae560a45dc3990e
kelly <kelly@kali>
2026-03-17 22:02:51 +0300

    Aloud hosts

--- MyTool/settings.py
+++ MyTool/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n-p1afq!@2sgp7%(x!-fzj+$rst8h#napw^y6sj8du=%y#x8$_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
